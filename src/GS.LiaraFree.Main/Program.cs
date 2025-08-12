ce2e8184df5940787bec6c1712465648e97070d3
Mohammad Babayi <chief.mb1976@gmail.com>
2025-08-12 11:18:37 +0330

    init: liara template

---
+++ src/GS.LiaraFree.Main/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// -----------------------------
+// Configuration & recommended services
+// -----------------------------
+builder.Services.Configure<ForwardedHeadersOptions>(a =>
+{
+    a.KnownNetworks.Clear();
+    a.KnownProxies.Clear();
+    a.ForwardedHeaders = ForwardedHeaders.All;
+});
+
+// -----------------------------
+// Build
+// -----------------------------
+var app = builder.Build();
+
+// -----------------------------
+// Middleware pipeline (order matters)
+// -----------------------------
+app.UseForwardedHeaders();
+app.UseRouting();
+app.UseEndpoints(_ => { });
+
+// -----------------------------
+// Endpoint mappings
+// -----------------------------
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
